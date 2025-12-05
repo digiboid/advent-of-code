dcf364631bcb998fd036a159dae90135a396e45c
digiboid <64439910+digiboid@users.noreply.github.com>
2025-12-05 08:45:28 +0100

    Work on day 5 part 1

--- 2025/day-5/src/main.rs
+++ 2025/day-5/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+fn parse(path: &str) {
+    let contents = fs::read_to_string(path).expect("Where's the file nanoda?");
+    let contents: Vec<&str> = contents.lines().collect();
+    dbg!(contents);
+}
+
 fn main() {
+    let contents = parse("test_input.txt");
     println!("Hello, world!");
 }
