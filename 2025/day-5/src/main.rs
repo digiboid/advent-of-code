f71a1f70de5228c2d0760f53de4e632ea27998f3
digiboid <64439910+digiboid@users.noreply.github.com>
2025-12-05 09:40:25 +0100

    Pass test input

--- 2025/day-5/src/main.rs
+++ 2025/day-5/src/main.rs
@@ -1,11 +1,50 @@
 use std::fs;
-fn parse(path: &str) {
+fn parse(path: &str) -> usize {
     let contents = fs::read_to_string(path).expect("Where's the file nanoda?");
-    let contents: Vec<&str> = contents.lines().collect();
-    dbg!(contents);
+    let mut contents: Vec<&str> = contents.lines().collect();
+    let mut index = 0;
+    for n in 0..contents.len() {
+        if contents[n] == "" {
+            index = n;
+        }
+    }
+    let ids = contents.split_off(index + 1);
+    let ids: Vec<usize> = ids.iter().map(|s| s.parse().unwrap()).collect();
+    // let id_ranges = contents.split_off(at)
+    let mut id_ranges = contents.clone();
+    id_ranges.pop();
+    let id_ranges: Vec<Vec<&str>> = id_ranges
+        .iter()
+        .map(|s| s.split("-").collect::<Vec<&str>>())
+        .collect();
+    let id_ranges: Vec<Vec<usize>> = id_ranges
+        .clone()
+        .iter()
+        .map(|r| {
+            let i1 = r[0].parse::<usize>().unwrap();
+            let i2 = r[1].parse::<usize>().unwrap();
+            (i1..=i2).collect()
+        })
+        .collect();
+    let fresh_ids: Vec<usize> = ids
+        .clone()
+        .iter()
+        .filter(|id| {
+            id_ranges
+                .clone()
+                .iter()
+                .any(move |range| range.contains(id))
+        })
+        .copied()
+        .collect();
+
+    dbg!(&id_ranges);
+    dbg!(&ids);
+    dbg!(&fresh_ids);
+    fresh_ids.iter().count()
 }
 
 fn main() {
-    let contents = parse("test_input.txt");
-    println!("Hello, world!");
+    let fresh_count = parse("test_input.txt");
+    println!("Fresh: {fresh_count}");
 }
